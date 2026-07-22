d2f9f53d375a8032ba8b54317d3179c4b93ebada
eduardo <eduardo.oliveira@lumahealth.io>
2026-07-22 20:48:29 -0300

    Add auth-email RabbitMQ consumer to notification service
    Consumes messages from the auth-email queue bound to the
    jobber-email-notification exchange and logs the parsed payload.

---
+++ server/2-notification-service/src/queues/email.consumer.ts
@@ -0,0 +1,31 @@
+import { winstonLogger } from '@edemuner/jobber-shared';
+import { Channel, ConsumeMessage } from 'amqplib';
+import { config } from '../config';
+import { Logger } from 'winston';
+import { createConnection } from './connection';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'emailConsumer', 'debug');
+
+async function consumeAuthEmailMessages(channel: Channel): Promise<void> {
+    try {
+        if(!channel){
+            channel = await createConnection() as Channel;
+        }
+
+        const exchangeName = 'jobber-email-notification';
+        const routingKey = 'auth-email';
+        const queueName = 'auth-email-queue';
+
+        await channel.assertExchange(exchangeName, 'direct');
+        const jobberQueue = await channel.assertQueue(queueName, { durable: true, autoDelete: false });
+
+        await channel.bindQueue(jobberQueue.queue, exchangeName, routingKey);
+        channel.consume(jobberQueue.queue, async (msg: ConsumeMessage | null) => {
+            console.log(JSON.parse(msg!.content.toString()))
+        })
+    } catch(error){
+        log.log('error', 'NotificationService EmailConsumer consumeAuthEmailMessages() method', error);
+    }
+} 
+
+export { consumeAuthEmailMessages }
